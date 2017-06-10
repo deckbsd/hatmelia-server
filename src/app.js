84673c475c31cc36fbf4d0917b232d11980d67f1
Julien Flawinne <cptflaw@outlook.com>
2017-06-10 22:42:41 +0200

    server skeleton

---
+++ src/app.js
@@ -0,0 +1,21 @@
+'use strict'
+let express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+var router = express.Router();
+
+var checkLinksController = (req, res) =>
+{
+    res.status(200);
+    res.send();  
+};
+
+router.route('/checklinks').get(checkLinksController);
+app.use('/api/v1', router);
+app.get('/', (req, res) => {
+    res.send('hatmelia api')
+});
+
+app.listen(port, _=>{
+    console.log('listening on port : ' + port);
+});
